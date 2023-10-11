956964387afb3c010830e576af9b7e2ab8999c89
paulvarkey758 <paulkv1997@gmail.com>
2023-10-11 16:31:18 +0530

    contact form validation updated

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ window.addEventListener("load",function(){
 });
 
 $(document).ready(function(){
-    $(".counter").counterUp({
-        delay:10,
-        time:1100
-    });
+    // $(".counter").counterUp({
+    //     delay:10,
+    //     time:1100
+    // });
     $(".menu-icon").click(function(){
         $(".navlist").toggleClass("navshow");
     });
@@ -33,20 +33,62 @@ function testimonialAnim(){
     }
 }
 
+const handleErrorMessage = (currentEl)=>{
+    newEl = document.createElement('div')
+    newEl.classList.add("form-validation-message")
+    newEl.innerHTML = "This field is required"
+    currentEl.parentNode.append(newEl)
+}
+
 $("#myForm").submit((e)=>{
     e.preventDefault()
-    $.ajax({
-        url:"https://script.google.com/macros/s/AKfycbyXvVoSDrm5DkXSnNWzMQDfHlBrC_BprdxN_ZrCrYXco61RyLjyWmXjL61S3ueQ8ZaA/exec",
-        data:$("#myForm").serialize(),
-        method:"post",
-        success:function (response){
-            alert("Form submitted successfully")
-            window.location.reload()
-            //window.location.href="https://google.com"
-        },
-        error:function (err){
-            alert("Something Error")
-
-        }
-    })
+    var parentName = document.getElementById("parentName")
+    var studentName = document.getElementById("studentName")
+    var studyingIn = document.getElementById("studyingIn")
+    var phone = document.getElementById("phone")
+    var email = document.getElementById("email")
+    var slot = document.getElementById("slot")
+    var course = document.getElementById("course")
+    if(parentName.value === ""){
+        handleErrorMessage(parentName) 
+    }
+    else if(studentName.value === ""){
+        handleErrorMessage(studentName) 
+    }
+    else if(studyingIn.value === ""){
+        handleErrorMessage(studyingIn) 
+    }
+    else if(phone.value === ""){
+        handleErrorMessage(phone) 
+    }
+    else if(email.value === ""){
+        handleErrorMessage(email) 
+    }
+    else if(slot.value === ""){
+        handleErrorMessage(slot) 
+    }
+    else if(course.value === ""){
+        handleErrorMessage(course) 
+    }
+    else{
+        document.getElementById("preloader").style.display = "block"
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycbyXvVoSDrm5DkXSnNWzMQDfHlBrC_BprdxN_ZrCrYXco61RyLjyWmXjL61S3ueQ8ZaA/exec",
+            data:$("#myForm").serialize(),
+            method:"post",
+            success:function (response){
+                // alert("Form submitted successfully")
+                console.log("Form submitted successfully")
+                document.getElementById("preloader").style.display = "none"
+                window.location.reload()
+                //window.location.href="https://google.com"
+            },
+            error:function (err){
+                alert("Something Error")
+    
+            }
+        })
+    }
+    
+    
 })
